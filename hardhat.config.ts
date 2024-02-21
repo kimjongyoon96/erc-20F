a51d05081b4993c41d40a4e6d66ee80d12acd407
kimjongyoon96 <kimjongyoon96@gmail.com>
2024-02-21 21:35:51 +0900

    테스트 통과 및 계약서 작성

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,9 @@ import dotenv from "dotenv";
 // const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+  },
 };
 
 export default config;
--- test/TestERC20.ts
+++ test/TestERC20.ts
@@ -1,24 +1,17 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { MyKarina } from "../typechain-types";
+import { KARINAToken } from "../typechain-types/contracts/myKarina.sol/KARINAToken";
 
-describe("mykarina contract", function () {
-  let myKarinaContract: MyKarina;
-  let owner: any;
-  let addr1: any;
+describe("KARINAToken contract", function () {
+  let karinaToken: KARINAToken;
 
-  //* beforeEach : 각 테스트 실행 전에 "myKarina" 스마트 계약을 배포하고 초기 설정을 수행
   beforeEach(async function () {
-    [owner, addr1] = await ethers.getSigners();
+    const karinaTokenFactory = await ethers.getContractFactory("KARINAToken");
+    karinaToken = (await karinaTokenFactory.deploy(10000000)) as KARINAToken;
+    await karinaToken.getDeployedCode();
+  });
 
-    //* myKarina 스마트 계약 배포, 본격적인 배포는 scripts 폴더 안에서 진행해야 한다.
-
-    const myKarinaFactory = await ethers.getContractFactory("myKarina");
-    myKarinaContract = (await myKarinaFactory.deploy(
-      "KarinaToken",
-      "KRN"
-    )) as MyKarina;
-    //* deployed() 함수가 안먹히는데, 이유를 파악해야 한다.  => wautFirDeployment()
-    myKarinaContract.waitForDeployment();
+  it("Should do something", async function () {
+    // 테스트 로직
   });
 });
